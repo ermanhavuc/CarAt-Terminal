c6ecd9d31983d5bd65f108c4a27e87be87195e36
berkayyaman95@gmail.com <ven4s3419/3>
2017-11-30 03:56:21 +0300

    Tüm argümanların kontrolü yapıldı.

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-#include <zconf.h>
 
 #define MAX_LINE 128 /* 80 chars per line, per command, should be enough. */
+#define NUM_OF_CMM 5 //defines total numbers of commands
 
 /* The setup function below will not return any value, but it will just: read
 in the next command line; separate it into distinct arguments (using blanks as
@@ -19,8 +19,11 @@ char cmm_codesearch[80]="codesearch";
 char cs_rec[3]="-r";
 char cmm_print[80]="print";
 char cmm_set[80]="set";
+char cmm_exit[80]="exit";
+char *cmm[NUM_OF_CMM];
+char eq_sign[2]="=";
 
-void setup(char inputBuffer[], char *args[],int *background)
+int setup(char inputBuffer[], char *args[],int *background)
 {
     int length, /* # of characters in the command line */
             i,      /* loop index for accessing inputBuffer array */
@@ -51,10 +54,8 @@ void setup(char inputBuffer[], char *args[],int *background)
     }
     //printf(">>%s<<",inputBuffer);
     int isQuote=0,eq_sign_b=0;
-    char eq_sign[2]="=";
     for (i=0;i<length;i++){ /* examine every character in the inputBuffer */
         switch (inputBuffer[i]){
-
             case '=':
                 eq_sign_b=1;
             case ' ':
@@ -95,26 +96,131 @@ void setup(char inputBuffer[], char *args[],int *background)
     }    /* end of for */
     args[ct] = NULL; /* just in case the input line was > 80 */
     //printf("%s",inputBuffer);
-    for (i = 0; i < ct; i++)
-        printf("args %d = %s ve %c\n",i,args[i],args[i][strlen(args[i])-1]);
-    //checkArgs(args,ct);
+    for (i = 0; i < ct; i++) {
+        int ret = checkArgs(args,ct,*background);
+        if(ret==-1) {
+            printf("----\n");
+            return -1;
+        }
+        printf("args %d = %s ve %c ve %d bg=%d\n",i,args[i],args[i][strlen(args[i])-1],ret,*background);
+    }
+
 } /* end of setup routine */
 
-int checkArgs(char *args[],int ct){
-    int needed=2;
+int checkArgs(char *args[],int ct,int background){
     ct-=1;
-    if(!strcmp(cmm_bookmark,args[0])){
-        needed=1;
-        if(!strcmp(bm_list,args[1])||(args[1][0]==34&&args[1][strlen(args[1])-1]==34))
-            needed=0;
-        else if(!strcmp(bm_dlt,args[1])||!strcmp(bm_idx,args[1]))
-            needed=1;
-        if(!needed){
+    int okay=0;
+    if(ct>=0){
+        if(!strcmp(cmm_bookmark,args[0])){
+            return check_for_bm(args,ct,background);
+        }else if(!strcmp(cmm_codesearch,args[0])){
+            return check_for_cs(args,ct,background);
+        }else if(!strcmp(cmm_print,args[0])){
+            return check_for_print(args,ct,background);
+        }else if(!strcmp(cmm_set,args[0])){
+            return check_for_set(args,ct,background);
+        }else if(!strcmp(cmm_exit,args[0])&&ct==0){
+            return -1;
+        }else okay=5; //wrong arguments
+    }else okay=4;//arguments not enough
+    return okay;
+}
+int check_for_print(char*args[],int ct,int background){
+    int okay=5;
+    if(ct>=1){
+        okay=check_if_bg(background,0,ct);
+        int i;
+        for(i=0;i<NUM_OF_CMM;i++){
+            if(!strcmp(args[1],cmm[i])){
+                if(ct==1) okay=1;
+                else okay=check_if_bg(background,1,ct);
+            }else if(okay!=1) okay=5;
+        }
+    }else okay=1;
+    return okay;
+}
+int check_for_set(char*args[],int ct,int background){
+    int okay=0;
+    if(ct>=3){
+        int i;
+
+        if(!strcmp(args[2],eq_sign)){
+            if(ct==3) okay=1;
+            else okay=check_if_bg(background,3,ct);
+        }else okay=5; //wrong arguments
+
+        for(i=0;i<NUM_OF_CMM;i++){
+            if(!strcmp(args[1],cmm[i])){
+                okay=0;
+                break;
+            }else okay=5;//wrong arguments
+        }
+    }else okay=4; //arguments not enough
+    return okay;
+}
+int check_for_cs(char*args[],int ct,int background){
+    int okay=0;
+    if(ct>=1){
+           if(!strcmp(args[1],cs_rec)){
+           if(ct>=2){
+               if(args[2][0]==34 && args[2][strlen(args[2]-1)]==34){
+                   if(ct==2) okay=1;
+                   else okay=check_if_bg(background,2,ct);
+               }else okay=6; //not a valid string
+           }else okay=4; //arguments not enough
+       }else if(args[1][0]==34 && args[2][strlen(args[1]-1)]==34){
+           if(ct==1) okay=1;
+           else okay=check_if_bg(background,1,ct);
+       }else okay=5; //wrong arguments
+    }else okay=4; //arguments not enough
+    return okay;
+}
+int check_for_bm(char*args[],int ct,int background){
+    int okay=0;
+
+        if(ct>=1){
+            if(!strcmp(bm_list,args[1])||(args[1][0]==34&&args[1][strlen(args[1])-1]==34)){
+                if(ct==1) okay=1;
+                else okay=check_if_bg(background,1,ct);
+            }
+            else if(!strcmp(bm_dlt,args[1])||!strcmp(bm_idx,args[1])){
+                if(ct>=2){
+                    if(check_if_int(args[2])){
+                        if(ct==2) okay=1;
+                        else okay=check_if_bg(background,2,ct);
+                    }else okay=3;//value is not integer
+                }else okay=4;//arguments not enough
+            }else okay=5;//wrong arguments
+        }else okay=4;//arguments not enough
+
+    return okay;
+}
+int check_if_int(char str[]){
+    int i,lnt=strlen(str);
+    for(i=0;i<lnt;i++){
+        if(!(str[i]>=48&&str[i]<=57)){
+            return 0;
         }
     }
+    return 1;
+}
+int check_if_bg(int background,int idx,int ct){
+    if(background==1&&(idx+1)==ct){
+        return 1;
+    }
+    return 2;
+}
+void initialize(){
+    cmm[0]=&cmm_bookmark;
+    cmm[1]=&cmm_codesearch;
+    cmm[2]=&cmm_print;
+    cmm[3]=&cmm_set;
+    cmm[4]=&cmm_exit;
 }
 int main(void)
 {
+    initialize();
+    int end =0;
     char inputBuffer[MAX_LINE]; /*buffer to hold command entered */
     int background; /* equals 1 if a command is followed by '&' */
     char *args[MAX_LINE/2 + 1]; /*command line arguments */
@@ -122,11 +228,14 @@ int main(void)
         background = 0;
         //printf("myshell: ");
         /*setup() calls exit() when Control-D is entered */
-        setup(inputBuffer, args, &background);
+        end=setup(inputBuffer, args, &background);
+        printf("***\n");
+        if(end==-1) break;
         /** the steps are:
         (1) fork a child process using fork()
         (2) the child process will invoke execv()
         (3) if background == 0, the parent will wait,
         otherwise it will invoke the setup() function again. */
     }
+    return end;
 }
