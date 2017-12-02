a712d2189f3065dd5d20fa39ee64db6acbae0eeb
berkayyaman95@gmail.com <ven4s3419/3>
2017-12-02 16:33:07 +0300

    io argümanları için kontrol fonksiyonu yazıldı fakat programa eklenmedi

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#define ioptlen 4
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
@@ -16,6 +17,79 @@ char **env;
 in the next command line; separate it into distinct arguments (using blanks as
 delimiters), and set the args array entries to point to the beginning of what
 will become null-terminated, C-style strings. */
+char *io_opt_o[];
+char io_o[40]="->\0>>\0>\0<";
+char *io_pt[ioptlen];
+
+void init_io_pts(){
+    io_pt[0]=&io_o[0];
+    io_pt[1]=&io_o[3];
+    io_pt[2]=&io_o[6];
+    io_pt[3]=&io_o[8];
+    /*int i;
+    for(i=0;i<4;i++){
+        printf("%s\n",io_pt[i]);
+    }*/
+}
+
+int check_io(char *args[],int ct,int cur,int background){
+
+    init_io_pts();
+    int i=cur,op=0;
+    int j=0,init_j=0,lim_j=4,tot_lim=2;
+    char temp[80]="";
+    while(args[i]!=NULL){
+
+
+        if(args[i+1]==NULL){
+            //printf("%d %s %d %d\n",args[i][0],io_pt[j],op,tot_lim);
+            if(args[i][0]==38&&background==1){
+
+                return 1;
+            }
+        }
+        strcpy(temp,io_pt[j]);
+        if(args[i][0]>=48&&args[i][0]<=57){
+            temp[0]=args[i][0];
+        }
+        //printf("-----%s\n",temp);
+        printf("1- %s %s %d %d %d\n",args[i],io_pt[j],op,tot_lim,i);
+        if(!strcmp(args[i],temp)&&op==0&&tot_lim){
+            printf("987977878798");
+            op=1;
+            if(j==0||j==1||j==2) {
+                init_j=3;
+                tot_lim=2;
+            }
+            else if(j==3){
+
+                init_j=0;
+                lim_j=3;
+                tot_lim=4;
+            }
+            tot_lim--;
+            j=init_j;
+            i++;
+        }else if(op==0){
+            j++;
+            if(j==lim_j){
+                return 5;
+            }
+        }
+        printf("2- %s %s %d %d %d\n",args[i],io_pt[j],op,tot_lim,i);
+        if(op==1&&access(args[i], F_OK)!=-1&&tot_lim) {
+            printf("ok\n");
+            op=0;
+            tot_lim--;
+            i++;
+        }else if(op==1){
+            printf("NO FILE EXISTS IN THAT NAME\n");
+            return 5;
+        }
+
+    }
+    return 1;
+}
 
 int setup(char inputBuffer[], char *args[],int *background)
 {
@@ -248,6 +322,7 @@ int check_for_cs(char*args[],int ct,int background){
            else okay=check_if_bg(background,1,ct);
        }else okay=5; //wrong arguments
     }else okay=4; //arguments not enough
+
     return okay;
 }
 
@@ -288,9 +363,22 @@ void initialize(){
 }
 int main(int x,char *y[],char **envp)
 {
+    char *asd[8];
+    char str0[]="<";
+    char str1[]="/home/berkay/Documents/asd.txt";
+    char str2[]="5>";
+    char str3[]="/home/berkay/Documents/asd.txt";
+    char str4[]="&";
+    asd[0]=str0;
+    asd[1]=str1;
+    asd[2]=str2;
+    asd[3]=str3;
+    asd[4]=str4;
+    asd[5]=NULL;
+    printf("%d",check_io(asd,3,0,1));
+    exit(0);
     env=envp;
     initialize();
-
     int end =0;
     char inputBuffer[MAX_LINE]; /*buffer to hold command entered */
     int background; /* equals 1 if a command is followed by '&' */
